4ab1044c85ac240257fcab7acc5a17284bb78b9f
kallinnas <kallinnas@gmail.com>
2024-09-04 22:29:40 +0300

    BE: fix bug with unitPrice

--- OrderManagerAPI/Data/MapperProfile.cs
+++ OrderManagerAPI/Data/MapperProfile.cs
@@ -11,7 +11,7 @@ public class MapperProfile : Profile
             .ForMember(des => des.EmployeeName, opt => opt.MapFrom(src => $"{src.Employee.FirstName} {src.Employee.LastName}"))
             .ForMember(des => des.CustomerName, opt => opt.MapFrom(src => src.Customer.Name))
             .ForMember(des => des.ShipperName, opt => opt.MapFrom(src => src.Shipper.Name))
-            .ForMember(des => des.OrderTotalPrice, opt => opt.MapFrom(src => src.OrderDetails.Sum(od => od.Product.Price * od.Product.Unit)))
+            .ForMember(des => des.OrderTotalPrice, opt => opt.MapFrom(src => src.OrderDetails.Sum(od => od.Product.Price * od.Quantity)))
             .ReverseMap();
 
         CreateMap<OrderDtoAdd, Order>()
